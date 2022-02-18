8ed5f1becd265b6c24b15557253db4d87ab67971
Jack McVeigh <jmcveigh55@gmail.com>
2022-02-17 20:56:52 -0500

    Refactoring API into rest api
    Signed-off-by: Jack McVeigh <jmcveigh55@gmail.com>

--- config/__init__.py
+++ config/__init__.py
@@ -5,10 +5,11 @@ BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 LOG_DIR = BASE_DIR + '/logs'
 os.makedirs(LOG_DIR, exist_ok=True)
 
-assert 'config.json' in os.listdir(f'{BASE_DIR}/config/'), \
+assert os.path.exists(f'{BASE_DIR}/config/config.json'), \
     'Cannot locate config.json'
 
 with open(f'{BASE_DIR}/config/config.json', 'r') as config_json:
     _config = json.load(config_json)
 
 API_HOST = _config['api']['host']
+API_PORT = _config['api']['port']
