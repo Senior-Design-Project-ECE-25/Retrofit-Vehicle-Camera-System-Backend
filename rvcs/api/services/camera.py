8167f0518be15581c713ba92fd5a04d4c83d1f6a
Jack McVeigh <jmcveigh55@gmail.com>
2022-04-26 15:39:29 -0400

    Trying to expose video as mp4

--- rvcs/api/services/camera.py
+++ rvcs/api/services/camera.py
@@ -45,7 +45,7 @@ class Camera:
         while True:
             frame = self.__get_frame()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+                   b'Content-Type: video/mp4\r\n\r\n' + frame + b'\r\n\r\n')
 
     def record(self):
         last = 0
